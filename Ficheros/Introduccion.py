ead406f4c7ea3a077f8900dadac7c99560af621e
LucasOChandoMonte <lucasochandomonte@gmail.com>
2025-03-06 21:11:24 +0100

    cambios

--- Ficheros/Introduccion.py
+++ Ficheros/Introduccion.py
@@ -2,6 +2,10 @@
     r -> lectura
     w -> escritura
     a -> anexado
+    r+ -> Lectura y escritura
+    w+ -> Lectura y escritura sin convervar el contenido
+    a+ -> Lectura y escritura anexado
+    x -> Abre el fichero para escritura SOLO si no existe, sino lanza ERROR
     rb, wb, ab -> modos binarios para trabajar con ficheros de texto    
 """
 
@@ -27,6 +31,15 @@
 
 # 4. Usar with para el manejo de ficheros
 #   Lo que hace es gestionar de forma automática la apertura y cierre de ficheros
+
+# 5. Funcion seek()
+#   Sintaxis-> file.seek(offset, whence) Donde offset es la distancia a la posicion indicada por whence-> whence = 0 (defecto) inicio, whence = 1 desde la posicion actual del puntero, whence = 2 desde el final del fichero
+
+# 6. Funcion tell() -> indica la posicion actual del puntero en el fichero
+#   fichero.tell()
+
+# 7. Funcion flush() -> fuerza el guardado de los datos en el fichero antes del close()
+#   fichero.flush()
 
 #EJEMPLO:
 
