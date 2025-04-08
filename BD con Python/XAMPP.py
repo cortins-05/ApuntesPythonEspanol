8b0fdd8d7030a8ed6ed1de9e5683cd7ee69e97dd
LucasOChandoMonte <lucasochandomonte@gmail.com>
2025-04-08 21:59:43 +0200

    hola

--- BD con Python/XAMPP.py
+++ BD con Python/XAMPP.py
@@ -6,22 +6,23 @@ pip install mysql-connector-python
 """
 
 #Conectar a MySQL
-import mysql.connector as conexionSQL
-conn = conexionSQL.connect(
-    host = "localhost",
-    user = "root",
-    password = "",
-    database = "ejemplo"
+import mysql.connector
+cnx = mysql.connector.connect(
+    host='localhost',       # El servidor MySQL en XAMPP
+    user='root',            # El usuario de MySQL
+    password='',           # La contraseña del usuario (por defecto está vacía)
+    database='ejemplo'     # El nombre de la base de datos
 )
+cursor = cnx.cursor()
 
-cursor = conn.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS ejemplo")
 
 #Crear una tabla en MySQL
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS personas (
-    id INT PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    edad INTEGER NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    edad INT NOT NULL
 )
 ''')
 
@@ -44,5 +45,5 @@ cursor.execute("UPDATE personas SET edad = 26 WHERE nombre = 'Carlos'")
 cursor.execute("DELETE FROM personas WHERE nombre = 'Marta'")
 
 #Cerrar la conexion
-conn.commit()
-conn.close()
+cnx.commit()
+cnx.close()
